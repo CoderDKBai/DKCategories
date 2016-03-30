80760a0129a86d15a2b8f71248eb2e4e618e5329
popo <popo@popodeiMac.local>
2016-03-30 13:25:01 +0800

    no message

--- DKCategories/DKCategories/DKCategories/UIButton/UIButton+DKAdd.h
+++ DKCategories/DKCategories/DKCategories/UIButton/UIButton+DKAdd.h
@@ -8,11 +8,26 @@
 
 #import <UIKit/UIKit.h>
 
+typedef NS_ENUM(NSInteger, DKImagePosition) {
+    DKImagePositionLeft = 0,    // 图片在左，文字在右，默认
+    DKImagePositionRight,       // 图片在右，文字在左
+    DKImagePositionTop,         // 图片在上，文字在下
+    DKImagePositionBottom,      // 图片在下，文字在上
+};
+
 @interface UIButton (DKAdd)
 
 /**
  *  扩大按钮的响应范围（insets必须不被button的superview给挡住）
  */
 @property (assign, nonatomic) UIEdgeInsets expandHitEdgeInsets;
+
+/**
+ *  利用 UIButton 的 titleEdgeInsets 和 imageEdgeInsets 来实现文字和图片的自由排列
+ *  注意：这个方法需要在设置图片和文字之后才可以调用，且 button 的大小要大于 图片大小+文字大小+spacing
+ *
+ *  @param spacing 图片和文字的间隔
+ */
+- (void)dk_setImagePosition:(DKImagePosition)postion spacing:(CGFloat)spacing;
 
 @end
--- DKCategories/DKCategories/DKCategories/UIButton/UIButton+DKAdd.m
+++ DKCategories/DKCategories/DKCategories/UIButton/UIButton+DKAdd.m
@@ -32,4 +32,46 @@
     return CGRectContainsPoint(bounds, point);
 }
 
+/**
+ *  利用 UIButton 的 titleEdgeInsets 和 imageEdgeInsets 来实现文字和图片的自由排列
+ *  注意：这个方法需要在设置图片和文字之后才可以调用，且 button 的大小要大于 图片大小+文字大小+spacing
+ *
+ *  @param spacing 图片和文字的间隔
+ */
+- (void)dk_setImagePosition:(DKImagePosition)postion spacing:(CGFloat)spacing {
+    
+    CGFloat imageWidth  = self.imageView.image.size.width;
+    CGFloat imageHeight = self.imageView.image.size.height;
+    CGFloat labelWidth  = [self.currentTitle sizeWithAttributes:@{NSFontAttributeName:self.titleLabel.font}].width;
+    CGFloat labelHeight = [self.currentTitle sizeWithAttributes:@{NSFontAttributeName:self.titleLabel.font}].height;
+    
+    CGFloat imageOffsetX = (imageWidth + labelWidth) / 2 - imageWidth / 2;
+    CGFloat imageOffsetY = imageHeight / 2 + spacing / 2;
+    CGFloat labelOffsetX = (imageWidth + labelWidth / 2) - (imageWidth + labelWidth) / 2;
+    CGFloat labelOffsetY = labelHeight / 2 + spacing / 2;
+    
+    switch (postion) {
+            
+        case DKImagePositionLeft:
+            self.imageEdgeInsets = UIEdgeInsetsMake(0, -spacing / 2, 0, spacing / 2);
+            self.titleEdgeInsets = UIEdgeInsetsMake(0, spacing / 2, 0, -spacing / 2);
+            break;
+            
+        case DKImagePositionRight:
+            self.imageEdgeInsets = UIEdgeInsetsMake(0, labelWidth + spacing / 2, 0, -(labelWidth + spacing / 2));
+            self.titleEdgeInsets = UIEdgeInsetsMake(0, -(imageHeight + spacing / 2), 0, imageHeight + spacing / 2);
+            break;
+            
+        case DKImagePositionTop:
+            self.imageEdgeInsets = UIEdgeInsetsMake(-imageOffsetY, imageOffsetX, imageOffsetY, -imageOffsetX);
+            self.titleEdgeInsets = UIEdgeInsetsMake(labelOffsetY, -labelOffsetX, -labelOffsetY, labelOffsetX);
+            break;
+            
+        case DKImagePositionBottom:
+            self.imageEdgeInsets = UIEdgeInsetsMake(imageOffsetY, imageOffsetX, -imageOffsetY, -imageOffsetX);
+            self.titleEdgeInsets = UIEdgeInsetsMake(-labelOffsetY, -labelOffsetX, labelOffsetY, labelOffsetX);
+            break;
+    }
+}
+
 @end
