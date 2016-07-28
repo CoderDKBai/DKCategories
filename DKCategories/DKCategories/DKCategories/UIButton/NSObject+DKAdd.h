cae6e77f5065bfcb19b7d26246ab2e4f485c4819
popo <popo@popodeiMac.local>
2016-07-28 16:00:43 +0800

    新增为模型属性添加默认值分类

---
+++ DKCategories/DKCategories/DKCategories/UIButton/NSObject+DKAdd.h
@@ -0,0 +1,17 @@
+//
+//  NSObject+DKAdd.h
+//  DKCategories
+//
+//  Created by popo on 16/7/28.
+//  Copyright © 2016年 CoderDKBai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (DKAdd)
+/**
+ * 给对象的属性设置默认值
+ */
+void checkEntity(NSObject *object);
+
+@end
